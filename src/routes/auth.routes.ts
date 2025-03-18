b10b115d48d0645bf2a8ed842eef17999f26afe4
Shahryar Khan <shahryar2k3@gmail.com>
2025-03-19 04:59:33 +0500

    User profile APIs

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,14 @@
 import { Hono } from "hono";
-import { register, login } from "../controllers/auth.controller.js";
-import { validateLogin, validateRegister } from "../validators/auth.validator.js";
+import { register, login, updateUserDetails, changePassword } from "../controllers/auth.controller.js";
+import { validateLogin, validatePasswordChange, validateRegister, validateUserUpdate } from "../validators/auth.validator.js";
+import { auth } from "../middleware/auth.middleware.js";
 
 const router = new Hono();
 
 router.post("/signup", validateRegister, register);
 router.post("/login", validateLogin, login);
+
+router.patch("/update-profile", auth, validateUserUpdate, updateUserDetails)
+router.patch("/change-password", auth, validatePasswordChange, changePassword)
 
 export default router;
